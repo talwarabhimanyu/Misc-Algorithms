55fddcd28843bb7804b0a3a8de2abde0453fc54d
Abhimanyu Talwar <p10abhimanyut@iima.ac.in>
2018-01-25 07:53:54 +0800

    Count inversions in O(N lgN) time.
    If i>j, and a[i] < a[j], then the pair (i,j) is an Inversion.

---
+++ CountInversion/CountInversion.java
@@ -0,0 +1,22 @@
+/** Abhimanyu Talwar                                                         **/
+/** https://github.com/talwarabhimanyu/                                      **/
+
+import edu.princeton.cs.algs4.StdIn; 
+import edu.princeton.cs.algs4.StdIn;
+
+public class CountInversion {
+    public static void main(String args[]){
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.readInt();
+        }
+        //Iterative Merge Sort routine which also counts the number of 
+        //inversions during the Merge operation.
+        int countInv = 0;
+        for (int i = 0; i < n; i++ {
+            
+        }
+    }
+}
